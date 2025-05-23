c8972af680574130445d2ca40f519a5c55189853
WESAM TAHA <tahawesam@yahoo.com>
2025-05-23 11:29:40 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -11,19 +11,6 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-          'survey-vendor': [
-            'survey-core',
-            'survey-creator-core',
-            'survey-creator-react',
-            'survey-react-ui'
-          ]
-        }
-      }
-    }
-  }
+  },
+  base: './',
 })
